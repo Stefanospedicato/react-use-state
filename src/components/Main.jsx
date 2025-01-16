da6f2511dbf3226f7ea83c2fe1e1cbaae92391f4
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-01-16 14:02:12 +0100

    feat: add onClick property

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,13 +2,24 @@ import { useState } from "react"
 import languages from "../../languages"
 
 const Main = () => {
+
+  const [selectedLanguage, setSelectedLanguage] = useState(null)
+
   return (
-    <div className="container d-flex justify-content-between my-5">
-      {languages.map(language => (
-        <button className="btn btn-primary">{language.title}</button>
-      ))}
-      <div></div>
-    </div>
+    <>
+      <div className="container d-flex justify-content-between my-5">
+        {languages.map(language => (
+          <button key={language.id} onClick={() => setSelectedLanguage(language)} className="btn btn-primary">{language.title}</button>
+        ))}
+      </div>
+      {selectedLanguage && (
+        <div className="container description">
+          <h3>{selectedLanguage.title}</h3>
+          <p>{selectedLanguage.description}</p>
+        </div>
+      )}
+
+    </>
   )
 }
 
